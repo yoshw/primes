137767b1a09ed3aec587d73fa0789e63fe659d49
Yoshua Wakeham <yoshwakeham@gmail.com>
2014-07-13 15:30:01 +1000

    Fixed error in is_naive_prime()

--- primes.py
+++ primes.py
@@ -77,7 +77,7 @@ def is_naive_prime(n):
     '''
     if n > 1:
         if n % 2 == 1:
-            for i in range(3, int(sqrt(n)), 2):
+            for i in range(3, int(sqrt(n))+1, 2):
                 if n % i == 0:
                     return False
             return True
